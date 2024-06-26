56e0141c8baaacbae23604a225fc6187790edf21
Cristian Durán <creature.secret@gmail.com>
2024-06-26 17:13:47 -0400

    add api service

---
+++ frontend/src/app/api-service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'http://localhost:8000/api/';
+  private clientes = 'clientes';
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<any> {
+    return this.http.get(`${this.apiUrl + this.clientes}`);
+  }
+}

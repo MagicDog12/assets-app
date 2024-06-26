07b874214616029a3d18952f4e8c209af4b24067
Cristian Durán <creature.secret@gmail.com>
2024-06-26 14:25:33 -0400

    add first view (clients list)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Cliente, Gestor, Pago
+from .serializers import ClienteSerializer, GestorSerializer, PagoSerializer
 
-# Create your views here.
+
+class ClienteList(generics.ListAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer

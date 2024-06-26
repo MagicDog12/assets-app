2bc8b4be4ac176386e8a6ce534a9ebfc7dca04ef
Cristian Durán <creature.secret@gmail.com>
2024-06-25 23:18:27 -0400

    add models to admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cliente, Gestor, Pago
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Gestor)
+admin.site.register(Pago)

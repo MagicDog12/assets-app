cae7c83c7850e856304e4272db01b22becc0a567
Cristian Durán <creature.secret@gmail.com>
2024-06-26 17:13:38 -0400

    add table in info clientes

--- frontend/src/app/info-clientes/info-clientes.component.ts
+++ frontend/src/app/info-clientes/info-clientes.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../api-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-info-clientes',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './info-clientes.component.html',
   styleUrl: './info-clientes.component.css'
 })
-export class InfoClientesComponent {
+export class InfoClientesComponent implements OnInit {
 
+  public data: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getClientes().subscribe({
+      next: (response) => {
+        this.data = response;
+        console.log(this.data);
+      },
+      error: (error) => {
+        console.error('Error fetching data', error);
+      },
+      complete: () => {
+        console.log('Data fetch complete');
+      }
+    });
+  }
 }

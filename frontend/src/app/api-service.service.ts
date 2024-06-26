7b62f685319f293ed30cf967b47ae1898753e2dc
Cristian Durán <creature.secret@gmail.com>
2024-06-26 17:37:19 -0400

    add info gestores

--- frontend/src/app/api-service.service.ts
+++ frontend/src/app/api-service.service.ts
@@ -9,10 +9,15 @@ export class ApiService {
 
   private apiUrl = 'http://localhost:8000/api/';
   private clientes = 'clientes';
+  private gestores = 'gestores';
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<any> {
     return this.http.get(`${this.apiUrl + this.clientes}`);
+  }
+
+  getGestores(nombre: string): Observable<any> {
+    return this.http.get(`${this.apiUrl + this.gestores}/?nombre=${nombre}`);
   }
 }
--- frontend/src/app/info-gestores/info-gestores.component.ts
+++ frontend/src/app/info-gestores/info-gestores.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../api-service.service';
+import { CommonModule } from '@angular/common';
+import { Gestor } from '../interfaces/gestor.interface';
+import { Cliente } from '../interfaces/client.interface';
 
 @Component({
   selector: 'app-info-gestores',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './info-gestores.component.html',
   styleUrl: './info-gestores.component.css'
 })
-export class InfoGestoresComponent {
+export class InfoGestoresComponent implements OnInit {
+  public clientes: Cliente[] = [];
+  public gestores: Gestor[] = [];
+  public clienteActual: string = '';
+  public show: boolean = false;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getClientes().subscribe({
+      next: (response) => {
+        this.clientes = response;
+        console.log(this.clientes);
+      },
+      error: (error) => {
+        console.error('Error fetching data', error);
+      },
+      complete: () => {
+        console.log('Data fetch complete');
+      }
+    });
+  }
+
+  update(nombre: string) {
+    this.apiService.getGestores(nombre).subscribe({
+      next: (response) => {
+        this.gestores = response;
+        console.log(this.gestores);
+      },
+      error: (error) => {
+        console.error('Error fetching data', error);
+      },
+      complete: () => {
+        console.log('Data fetch complete');
+        this.clienteActual = nombre;
+        this.show = true;
+      }
+    });
+  }
 }

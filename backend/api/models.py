0b0ef08d3f3f57012168e90314a0b6c211f9cbfd
Cristian Durán <creature.secret@gmail.com>
2024-06-26 14:13:34 -0400

    add comments to models

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,26 +4,27 @@ from django.db import models
 
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=10)  # Nombre del cliente (cartera)
 
     def __str__(self):
         return self.nombre
 
 
 class Gestor(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)  # Nombre del gestor
 
     def __str__(self):
         return self.nombre
 
 
 class Pago(models.Model):
-    fecha = models.DateField()
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    monto = models.IntegerField()
+    fecha = models.DateField()  # Fecha de pago
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # Cliente
+    monto = models.IntegerField()  # Monto del pago
+    # Gestor que realizó la gestión
     gestor = models.ForeignKey(Gestor, on_delete=models.CASCADE)
-    rut = models.IntegerField()
-    abono = models.IntegerField()
+    rut = models.IntegerField()  # Numero de identificacin del deudor
+    abono = models.IntegerField()  # Total de abonos realizados
 
     def __str__(self):
         return f'Pago con rut: {self.rut} y fecha: {self.fecha}'

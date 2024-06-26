571f45af88f8acbec631b7a0cf002dc4ee871cbd
Cristian Durán <creature.secret@gmail.com>
2024-06-26 15:57:34 -0400

    add views

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,3 +6,35 @@ from .serializers import ClienteSerializer, GestorSerializer, PagoSerializer
 class ClienteList(generics.ListAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
+
+class GestoresListByCliente(generics.ListAPIView):
+    def get_queryset(self):
+        nombre_cliente = self.request.query_params.get('nombre')
+        cliente = Cliente.objects.get(nombre=nombre_cliente)
+        pagos_queryset = Pago.objects.filter(cliente=cliente.id)
+        pagos = PagoSerializer(pagos_queryset, many=True)
+        gestores = set()
+        for pago in pagos.data:
+            gestor = Gestor.objects.get(id=pago['gestor'])
+            gestores.add(gestor)
+        return gestores
+    serializer_class = GestorSerializer
+
+
+class PagosByGestor(generics.ListAPIView):
+    def get_queryset(self):
+        nombre_gestor = self.request.query_params.get('nombre')
+        gestor = Gestor.objects.get(nombre=nombre_gestor)
+        pagos_queryset = Pago.objects.filter(gestor=gestor.id)
+        return pagos_queryset
+    serializer_class = PagoSerializer
+
+
+class PagosByCliente(generics.ListAPIView):
+    def get_queryset(self):
+        nombre_cliente = self.request.query_params.get('nombre')
+        cliente = Cliente.objects.get(nombre=nombre_cliente)
+        pagos_queryset = Pago.objects.filter(cliente=cliente.id)
+        return pagos_queryset
+    serializer_class = PagoSerializer

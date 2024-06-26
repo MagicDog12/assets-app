d48c2549a47d65feee50ea46a30c34328f0b62a5
Cristian Durán <creature.secret@gmail.com>
2024-06-26 15:57:26 -0400

    add urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,9 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('clientes/', views.ClienteList.as_view(), name='cliente-list'),
+    path('gestores/', views.GestoresListByCliente.as_view(),
+         name='gestores-list-by-cliente'),
+    path('pagos/cliente/', views.PagosByCliente.as_view(),
+         name='pagos-por-cliente'),
+    path('pagos/gestor/', views.PagosByGestor.as_view(), name='pagos-por-gestor')
 ]
